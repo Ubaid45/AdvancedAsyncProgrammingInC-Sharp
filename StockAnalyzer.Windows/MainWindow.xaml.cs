26142e30123cb2c0c606cb37026673855dce9e2e
Ubaid Rana <ubaidrana45@gmail.com>
2020-12-24 13:58:30 +0100

    Loaded large file synchronously

--- StockAnalyzer.Windows/MainWindow.xaml.cs
+++ StockAnalyzer.Windows/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using StockAnalyzer.Core.Domain;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading;
@@ -25,13 +27,22 @@ namespace StockAnalyzer.Windows
 
 
 
-        private async void Search_Click(object sender, RoutedEventArgs e)
+        private void Search_Click(object sender, RoutedEventArgs e)
         {
             try
             {
                 BeforeLoadingStockData();
-                var data = GetStocks();
-                await data;
+
+                var lines = File.ReadAllLines("StockPrices_Small.csv");
+                var data = new List<StockPrice>();
+
+                foreach(var line in lines.Skip(1))
+                {
+                    var price = StockPrice.FromCSV(line);
+                    data.Add(price);
+                }
+
+                Stocks.ItemsSource = data.Where(sp => sp.Identifier == StockIdentifier.Text);
             }
             catch(Exception ex)
             {
@@ -44,26 +55,6 @@ namespace StockAnalyzer.Windows
 
 
         }
-
-        private async Task GetStocks()
-        {
-            try
-            {
-                var store = new DataStore();
-                var responseTask = store.GetStockPrices(StockIdentifier.Text);
-                Stocks.ItemsSource = await responseTask;
-            }
-
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-
-
-
-
 
         private void BeforeLoadingStockData()
         {
